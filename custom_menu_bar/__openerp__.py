668ad397cdebebf404e3263574e109bf21f3b08e
Eneldo Serrata <eneldoserrata@gmail.com>
2016-04-14 14:18:05 -0400

    IMP

---
+++ custom_menu_bar/__openerp__.py
@@ -0,0 +1,16 @@
+{
+    'name' : 'Custom menu bar (orange)',
+    'version' : '1.0.0',
+    'author' : 'IT-Projects LLC, Ivan Yelizariev',
+    'license': 'GPL-3',
+    'category' : 'Tools',
+    'website' : 'https://yelizariev.github.io',
+    'description': """
+
+    """,
+    'depends' : ['base'],
+    'data':[
+        'views.xml',
+        ],
+    'installable': True
+}
